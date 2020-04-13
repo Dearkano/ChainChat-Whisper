bbddb5602cf4aad979fce110c4d29f807108be0d
Dearkano <vaynetian@cc98.org>
2020-04-13 20:35:53 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+## ChainChat
+
+**前端部分fork自<https://github.com/aermin/ghChat>**
---
+++ src/components/SignInSignUp/index.js
@@ -0,0 +1,128 @@
+import React, { Component } from 'react';
+import './index.scss';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Request from '../../utils/request';
+import Spinner from '../Spinner';
+import UserAvatar from '../UserAvatar';
+
+export default class SignInSignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      password: '',
+      showSpinner: true,
+    };
+  }
+
+  handleChange = event => {
+    const { target } = event;
+    this.setState({ [target.name]: target.value });
+  };
+
+  async loginGithub() {
+    const href = window.location.href;
+    if (/\/login\?code/.test(href)) {
+      const code = href.split('?code=')[1];
+      Request.axios('post', '/api/v1/github_oauth', {
+        code,
+        clientId: this.clientId,
+      })
+        .then(response => {
+          localStorage.setItem('userInfo', JSON.stringify(response));
+          window.location.reload();
+          const originalLink = sessionStorage.getItem('originalLink');
+          if (originalLink) {
+            sessionStorage.removeItem('originalLink');
+            window.location.href = originalLink;
+            return;
+          }
+          window.location.href = '/';
+        })
+        .catch(error => {
+          console.log(
+            '使用github登录前请确定你的github设置了public的email，否则可能会失败 error => ',
+            error,
+          );
+          window.open(
+            'https://user-images.githubusercontent.com/24861316/75133098-6b564600-5714-11ea-824a-b367ed55b1a1.png',
+          );
+          window.location.href = '/login';
+        });
+    }
+  }
+
+  componentDidMount() {
+    this.loginGithub().then(() => {
+      this.setState({ showSpinner: false });
+    });
+  }
+
+  handleClick = () => {
+    this.props.setValue(this.state);
+  };
+
+  get clientId() {
+    return '8c694af835d62f8fd490';
+  }
+
+  render() {
+    const { isRegister } = this.props;
+    const { name, password } = this.state;
+    const loginClass = isRegister ? 'active' : 'inactive';
+    const registerClass = isRegister ? 'inactive' : 'active';
+    const linkUrl = isRegister ? '/register' : '/login';
+    const buttonName = isRegister ? '登录' : '注册';
+    const OAuthHref = `https://github.com/login/oauth/authorize?client_id=${this.clientId}`;
+    return (
+      <div className="formContent fadeInDown">
+        {this.state.showSpinner && <Spinner />}
+        <div className="ghChatLogo">
+          <img src="https://cdn.aermin.top/ghChatIcon.png" alt="ghChatLogo" />
+        </div>
+        <Link to={linkUrl}>
+          <span className={loginClass}>登录</span>
+        </Link>
+        <Link to={linkUrl}>
+          <span className={registerClass}>注册</span>
+        </Link>
+        <div className="userAvatarWrapper">
+          <UserAvatar name={name || 'Ÿ'} size="100" />
+        </div>
+        <div className="center">
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={this.handleChange}
+            placeholder="用户名"
+          />
+        </div>
+        <div className="center">
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={this.handleChange}
+            placeholder="密码"
+          />
+        </div>
+        <div className="center">
+          <input type="button" onClick={this.handleClick} value={buttonName} />
+        </div>
+        <div className="version">Version: 0.0.1</div>
+      </div>
+    );
+  }
+}
+
+SignInSignUp.propTypes = {
+  setValue: PropTypes.func,
+  isRegister: PropTypes.bool,
+};
+
+SignInSignUp.defaultProps = {
+  setValue() {},
+  isRegister: false,
+};
